a940a2b53b2b363735ea22830f201adf4e1dcb99
Alexander Gerlach <alexgerlach90@gmail.com>
2021-11-26 21:19:47 +0100

    fix: finalize keycloak implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import {
-  KeycloakEvent,
-  KeycloakEventType,
-  KeycloakService,
-} from 'keycloak-angular';
 import { Subject } from 'rxjs';
-import { exhaustMap, mergeMap, takeUntil } from 'rxjs/operators';
+import { takeUntil } from 'rxjs/operators';
 import { AuthService } from './auth/auth.service';
 
 @Component({
@@ -17,17 +12,11 @@ export class AppComponent implements OnInit, OnDestroy {
   title: string = 'fit-template';
   destroy$: Subject<void> = new Subject<void>();
   onTestGetRequest$: Subject<void> = new Subject<void>();
-  onGetToken$: Subject<void> = new Subject<void>();
 
-  constructor(
-    private authService: AuthService,
-    private keycloakService: KeycloakService
-  ) {}
+  constructor(private authService: AuthService) {}
 
-  ngOnInit() {
-    this.startKeycloakEventsSub();
-    this.startGetTokenSub();
-    this.startTestGetRequestSub();
+  async ngOnInit() {
+    this.authService.setCurrentUser();
   }
 
   ngOnDestroy() {
@@ -35,57 +24,31 @@ export class AppComponent implements OnInit, OnDestroy {
     this.destroy$.complete();
   }
 
-  startKeycloakEventsSub(): void {
-    this.keycloakService.keycloakEvents$
-      .pipe(takeUntil(this.destroy$))
-      .subscribe({
-        next: (event: KeycloakEvent) => {
-          if (event.type == KeycloakEventType.OnAuthError) {
-            console.log(event);
-          }
-          if (event.type == KeycloakEventType.OnAuthLogout) {
-            console.log(event);
-          }
-          if (event.type == KeycloakEventType.OnAuthRefreshError) {
-            console.log(event);
-          }
-          if (event.type == KeycloakEventType.OnAuthRefreshSuccess) {
-            console.log(event);
-          }
-          if (event.type == KeycloakEventType.OnReady) {
-            console.log(event);
-          }
-          if (event.type == KeycloakEventType.OnTokenExpired) {
-            this.keycloakService.updateToken(20);
-          }
-        },
-      });
+  async onGetToken() {
+    await this.authService.getToken();
   }
 
-  startGetTokenSub(): void {
-    this.onGetToken$
-      .pipe(
-        mergeMap(() => this.authService.getToken()),
-        takeUntil(this.destroy$)
-      )
-      .subscribe(console.log, console.error, () => 'getToken complete!');
-  }
-
-  startTestGetRequestSub(): void {
-    this.onTestGetRequest$
-      .pipe(
-        exhaustMap(() => this.authService.testGetRequest()),
-        takeUntil(this.destroy$)
-      )
-      .subscribe(console.log, console.error, () => 'testGetRequest complete!');
+  onClearToken() {
+    this.authService.clearToken();
   }
 
   onLogin(): void {
     this.authService.login();
   }
 
-  onLogout(): void {
-    this.authService.logout();
+  onTestGetRequest() {
+    this.authService
+      .testGetRequest()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(console.log);
+  }
+
+  async onLogout(): Promise<void> {
+    await this.authService.logout();
+  }
+
+  onSetCurrentUser() {
+    this.authService.setCurrentUser();
   }
 
   onGetRoles(): void {
@@ -93,6 +56,6 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   onGetLoggedUser(): void {
-    this.authService.getLoggedUser();
+    console.log(this.authService.currentUser);
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,59 +1,103 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { KeycloakService } from 'keycloak-angular';
-import { BehaviorSubject, Subscription } from 'rxjs';
+import {
+  KeycloakEvent,
+  KeycloakEventType,
+  KeycloakService,
+} from 'keycloak-angular';
+import { KeycloakTokenParsed } from 'keycloak-js';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  keycloakIsDown$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
-    false
-  );
-  keycloakEvents$: Subscription = new Subscription();
+  private currentUserSource$: BehaviorSubject<KeycloakTokenParsed | undefined> =
+    new BehaviorSubject<KeycloakTokenParsed | undefined>(this.getCurrentUser());
+  currentUser$ = this.currentUserSource$.asObservable();
+  set currentUser(user: KeycloakTokenParsed | undefined) {
+    this.currentUserSource$.next(this.getCurrentUser());
+  }
+  get currentUser(): KeycloakTokenParsed | undefined {
+    return this.getCurrentUser();
+  }
+
+  keycloakEventsSub$: Subscription = new Subscription();
 
   constructor(
     private http: HttpClient,
     private keycloakService: KeycloakService
-  ) {}
-
-  getLoggedUser() {
-    try {
-      let userDetails =
-        this.keycloakService.getKeycloakInstance().idTokenParsed;
-      console.log('UserDetails', userDetails);
-      console.log('UserRoles', this.keycloakService.getUserRoles());
-      return userDetails;
-    } catch (e) {
-      console.log('getLoggedUser Exception', e);
-      return undefined;
-    }
+  ) {
+    this.startKeycloakEventsSub();
   }
 
-  login() {
+  startKeycloakEventsSub(): void {
+    this.keycloakEventsSub$.unsubscribe();
+    this.keycloakEventsSub$ = this.keycloakService.keycloakEvents$.subscribe({
+      next: (event: KeycloakEvent) => {
+        // if (event.type == KeycloakEventType.OnAuthError) {
+        //   this.setCurrentUser();
+        // }
+        // if (event.type == KeycloakEventType.OnAuthLogout) {
+        //   this.setCurrentUser();
+        // }
+        // if (event.type == KeycloakEventType.OnAuthRefreshError) {
+        //   this.setCurrentUser();
+        // }
+        // if (event.type == KeycloakEventType.OnAuthRefreshSuccess) {
+        //   this.setCurrentUser();
+        // }
+        // if (event.type == KeycloakEventType.OnReady) {
+        //   this.setCurrentUser();
+        // }
+        if (event.type == KeycloakEventType.OnTokenExpired) {
+          this.keycloakService.updateToken(20);
+          // this.setCurrentUser();
+        }
+
+        this.setCurrentUser();
+      },
+    });
+  }
+
+  login(): void {
     this.keycloakService.login();
   }
 
-  logout() {
-    this.keycloakService.logout();
+  async logout(): Promise<void> {
+    await this.keycloakService.logout();
   }
 
-  redirectToProfile() {
+  clearToken(): void {
+    this.keycloakService.clearToken();
+  }
+
+  setCurrentUser(): void {
+    this.currentUser = this.getCurrentUser();
+  }
+
+  getCurrentUser() {
+    return this.keycloakService.getKeycloakInstance().idTokenParsed;
+  }
+
+  redirectToProfile(): void {
     this.keycloakService.getKeycloakInstance().accountManagement();
   }
 
   getUserRoles(): string[] {
     const roles = this.keycloakService.getUserRoles();
     console.log(roles);
-
     return roles;
   }
 
-  getToken() {
-    return this.keycloakService.getToken();
+  async getToken(): Promise<string> {
+    const token = await this.keycloakService?.getToken();
+    console.log(token);
+    return token;
   }
 
-  testGetRequest() {
-    return this.http.get('https://httpbin.org/get');
+  testGetRequest(): Observable<any> {
+    return this.http.get('https://httpbin.org/get').pipe(take(1));
   }
 }
