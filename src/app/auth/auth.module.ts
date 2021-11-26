9c9daabaa23bb24668141dbcc3796f6d82f52b63
Alexander Gerlach <alexgerlach90@gmail.com>
2021-11-26 15:09:32 +0100

    fix: add keycloak config

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,14 +2,19 @@ import { CommonModule } from '@angular/common';
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { KeycloakService } from 'keycloak-angular';
 import { of } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import * as keycloakConfig from 'src/assets/keycloak.config.json';
 import { AuthService } from './auth.service';
 
 const initializeKeycloak = (keycloak: KeycloakService) => {
+  const { realm, url, clientId } = keycloakConfig;
   return (): Promise<boolean> =>
     keycloak
       .init({
-        config: environment.keycloakConfig,
+        config: {
+          realm,
+          url,
+          clientId,
+        },
         initOptions: {
           onLoad: 'check-sso',
           silentCheckSsoRedirectUri:
